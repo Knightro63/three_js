07b4e892583bdefe855915c0d39148e0c63f6916
Knightro <50028636+Knightro63@users.noreply.github.com>
2026-01-16 16:20:35 -0500

    added audio properties

--- packages/three_js_core/lib/audio/audio.dart
+++ packages/three_js_core/lib/audio/audio.dart
@@ -5,9 +5,9 @@ import 'package:three_js_core/three_js_core.dart';
 ///
 /// You can use as a helper to very simply play a sound or a background music.
 /// Alternatively you can create your own instances and control them yourself.
-/// 
+///
 /// Please Use "VideoAudio, FlutterAudio, or AudioLatency"
-abstract class Audio extends Object3D{
+abstract class Audio extends Object3D {
   bool autoplay;
   bool loop;
   bool hasPlaybackControl;
@@ -18,77 +18,135 @@ abstract class Audio extends Object3D{
   bool get isPlaying => false;
   String path;
 
-  Audio({
-    required this.path,
-    this.playbackRate = 1.0,
-    this.hasPlaybackControl = true,
-    this.autoplay = false,
-    this.loop = false
-  });
+  Audio(
+      {required this.path,
+      this.playbackRate = 1.0,
+      this.hasPlaybackControl = true,
+      this.autoplay = false,
+      this.loop = false});
 
   /// Plays a single run of the given [file], with a given [volume].
-  Future<void> play([int delay = 0]) async{
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  Future<void> play([int delay = 0]) async {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
   }
 
-  Future<void> replay() async{
-    throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  Future<void> replay() async {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
   }
-
 
   /// Stops the currently playing background music track (if any).
   Future<void> stop() async {
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
   }
 
   /// Resumes the currently played (but resumed) background music.
   Future<void> resume() async {
-    throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
   }
 
   /// Pauses the background music without unloading or resetting the audio
   /// player.
   Future<void> pause() async {
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
   }
 
-	double? getPlaybackRate() {
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  double? getPlaybackRate() {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
 
-	void setPlaybackRate(double value){
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  void setPlaybackRate(double value) {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
 
-	bool getLoop() {
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  bool getLoop() {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
 
-	void setLoop(bool value ){
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  void setLoop(bool value) {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
 
-	void setLoopStart(int value ) {
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  void setLoopStart(int value) {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
 
-	void setLoopEnd(int value ) {
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  void setLoopEnd(int value) {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
 
-	double? getBalance() {
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  double? getBalance() {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
 
-	void setBalance(double value ){
-    throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  void setBalance(double value) {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
 
-	double? getVolume() {
-		throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  double? getVolume() {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
 
-	void setVolume(double value ){
-    throw('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
-	}
+  void setVolume(double value) {
+    throw ('Currently Not Implimented! Please Use "VideoAudio, FlutterAudio, or AudioLatency".');
+  }
+
+  /// [meta] - object containing metadata such as textures or images for the scene.
+  ///
+  /// Convert the scene to three.js
+  /// [JSON Object/Scene format](https://github.com/mrdoob/three.js/wiki/JSON-Object-Scene-format-4).
+  @override
+  Map<String, dynamic> toJson({Object3dMeta? meta}) {
+    Map<String, dynamic> data = super.toJson(meta: meta);
+
+    data['autoplay'] = autoplay;
+    data['loop'] = loop;
+    data['hasPlaybackControl'] = hasPlaybackControl;
+    data['loopEnd'] = loopEnd;
+    data['loopStart'] = loopStart;
+    data['playbackRate'] = playbackRate;
+    data['path'] = path;
+
+    return data;
+  }
+
+  @override
+  dynamic getProperty(String propertyName, [int? offset]) {
+    if (propertyName == 'autoplay') {
+      return autoplay;
+    } else if (propertyName == 'loop') {
+      return loop;
+    } else if (propertyName == 'hasPlaybackControl') {
+      return hasPlaybackControl;
+    } else if (propertyName == 'loopEnd') {
+      return loopEnd;
+    } else if (propertyName == 'loopStart') {
+      return loopStart;
+    } else if (propertyName == 'playbackRate') {
+      return playbackRate;
+    } else if (propertyName == 'path') {
+      return path;
+    }
+    return super.getProperty(propertyName, offset);
+  }
+
+  @override
+  Audio setProperty(String propertyName, dynamic value, [int? offset]) {
+    if (propertyName == 'autoplay') {
+      autoplay = value;
+    } else if (propertyName == 'loop') {
+      loop = value;
+    } else if (propertyName == 'loopEnd') {
+      loopEnd = value.toInt();
+    } else if (propertyName == 'loopStart') {
+      loopStart = value.toInt();
+    } else if (propertyName == 'playbackRate') {
+      playbackRate = value.toDouble();
+    } else if (propertyName == 'path') {
+      path = value;
+    } else {
+      super.setProperty(propertyName, value);
+    }
+
+    return this;
+  }
 }
--- packages/three_js_core/lib/audio/positional_audio.dart
+++ packages/three_js_core/lib/audio/positional_audio.dart
@@ -7,7 +7,7 @@ final _quaternion = Quaternion.identity();
 final _scale = Vector3.zero();
 final _orientation = Vector3.zero();
 
-class _AudioArea{
+class _AudioArea {
   _AudioArea(this.inside, [this.angle = 0, this.side = 0]);
 
   bool inside;
@@ -19,7 +19,7 @@ extension Ex on double {
   double toPrecision(int n) => double.parse(toStringAsFixed(n));
 }
 
-class PositionalAudio extends Object3D{
+class PositionalAudio extends Object3D {
   Audio audioSource;
   double refDistance;
   double maxDistance;
@@ -31,7 +31,7 @@ class PositionalAudio extends Object3D{
 
   Object3D listner;
 
-	PositionalAudio({
+  PositionalAudio({
     required this.audioSource,
     required this.listner,
     this.refDistance = 0,
@@ -42,38 +42,48 @@ class PositionalAudio extends Object3D{
     this.rolloffFactor = 3,
   });
 
-	void setDirectionalCone(double coneInnerAngle, double coneOuterAngle, double coneOuterGain) {
-		this.coneInnerAngle = coneInnerAngle;
-		this.coneOuterAngle = coneOuterAngle;
-		this.coneOuterGain = coneOuterGain;
-	}
+  void setDirectionalCone(
+      double coneInnerAngle, double coneOuterAngle, double coneOuterGain) {
+    this.coneInnerAngle = coneInnerAngle;
+    this.coneOuterAngle = coneOuterAngle;
+    this.coneOuterGain = coneOuterGain;
+  }
 
-  _AudioArea _insideAudioArea(Vector3 audioPos, Vector3 audioDirection, Vector3 objectPos, double angle){
-    if(angle <= 180){
+  _AudioArea _insideAudioArea(Vector3 audioPos, Vector3 audioDirection,
+      Vector3 objectPos, double angle) {
+    if (angle <= 180) {
       final normalizedDir = audioDirection.normalize();
       final objVector = objectPos.sub(audioPos);
       final disOnViewAxis = objVector.dot(normalizedDir);
       if (disOnViewAxis < 0.0) return _AudioArea(false);
       final temp = objVector.cross(normalizedDir).length;
-      final theta = math.atan(temp/disOnViewAxis);
-      final sign = Plane().setFromCoplanarPoints(Vector3(), objectPos, audioPos).normal.z.sign;
-      if(angle == 180 || maxDistance == double.maxFinite) return _AudioArea(true,theta.toDeg(),sign);
-      final r = math.tan((angle/2).toRad())*disOnViewAxis;
-      return _AudioArea(temp <= r,theta.toDeg(),sign);
-    }
-    else{
+      final theta = math.atan(temp / disOnViewAxis);
+      final sign = Plane()
+          .setFromCoplanarPoints(Vector3(), objectPos, audioPos)
+          .normal
+          .z
+          .sign;
+      if (angle == 180 || maxDistance == double.maxFinite)
+        return _AudioArea(true, theta.toDeg(), sign);
+      final r = math.tan((angle / 2).toRad()) * disOnViewAxis;
+      return _AudioArea(temp <= r, theta.toDeg(), sign);
+    } else {
       final normalizedDir = audioDirection.scale(-1).normalize();
       final objVector = objectPos.sub(audioPos);
       final disOnViewAxis = objVector.dot(normalizedDir);
       if (disOnViewAxis < 0.0) return _AudioArea(true);
       final temp = objVector.cross(normalizedDir).length;
-      final r = math.tan((angle/2).toRad())*disOnViewAxis;
-      final theta = math.atan(temp/disOnViewAxis);
-      final sign = Plane().setFromCoplanarPoints(Vector3(), objectPos, audioPos).normal.z.sign;
-      return _AudioArea(temp > r,theta.toDeg(),sign);
+      final r = math.tan((angle / 2).toRad()) * disOnViewAxis;
+      final theta = math.atan(temp / disOnViewAxis);
+      final sign = Plane()
+          .setFromCoplanarPoints(Vector3(), objectPos, audioPos)
+          .normal
+          .z
+          .sign;
+      return _AudioArea(temp > r, theta.toDeg(), sign);
     }
   }
-  
+
   bool get hasPlaybackControl => audioSource.hasPlaybackControl;
   bool get isPlaying => audioSource.isPlaying;
   Matrix4 get matrixWorld => audioSource.matrixWorld;
@@ -84,33 +94,39 @@ class PositionalAudio extends Object3D{
   double get playbackRate => audioSource.playbackRate;
   String get path => audioSource.path;
 
-  set hasPlaybackControl(bool value){
+  set hasPlaybackControl(bool value) {
     audioSource.hasPlaybackControl = value;
   }
-  set autoplay(bool value){
+
+  set autoplay(bool value) {
     audioSource.autoplay = value;
   }
-  set loop(bool value){
+
+  set loop(bool value) {
     audioSource.loop = value;
   }
-  set loopEnd(int value){
+
+  set loopEnd(int value) {
     audioSource.loopEnd = value;
   }
-  set loopStart(int value){
+
+  set loopStart(int value) {
     audioSource.loopStart = value;
   }
-  set playbackRate(double value){
+
+  set playbackRate(double value) {
     audioSource.playbackRate = value;
   }
-  set path(String value){
+
+  set path(String value) {
     audioSource.path = value;
   }
 
-  Future<void> play([int delay = 0]) async{
+  Future<void> play([int delay = 0]) async {
     await audioSource.play(delay);
   }
 
-  Future<void> replay() async{
+  Future<void> replay() async {
     await audioSource.replay();
   }
 
@@ -126,117 +142,213 @@ class PositionalAudio extends Object3D{
     await audioSource.pause();
   }
 
-	double? getPlaybackRate() {
+  double? getPlaybackRate() {
     return audioSource.getPlaybackRate();
   }
 
-	void setPlaybackRate(double value){
+  void setPlaybackRate(double value) {
     audioSource.setPlaybackRate(value);
   }
 
-	bool getLoop() {
+  bool getLoop() {
     return audioSource.getLoop();
   }
 
-	void setLoop(bool value ){
+  void setLoop(bool value) {
     audioSource.setLoop(value);
   }
 
-	void setLoopStart(int value ) {
-		audioSource.setLoopStart(value);
-	}
+  void setLoopStart(int value) {
+    audioSource.setLoopStart(value);
+  }
 
-	void setLoopEnd(int value ) {
-		audioSource.setLoopEnd(value);
-	}
+  void setLoopEnd(int value) {
+    audioSource.setLoopEnd(value);
+  }
 
-	double? getBalance() {
-		return audioSource.getBalance();
-	}
+  double? getBalance() {
+    return audioSource.getBalance();
+  }
 
-	void setBalance(double value ){
+  void setBalance(double value) {
     audioSource.setBalance(value);
-	}
+  }
 
-	double? getVolume() {
-		return audioSource.getVolume();
-	}
+  double? getVolume() {
+    return audioSource.getVolume();
+  }
 
-  void setVolume(double volume){
+  void setVolume(double volume) {
     print(volume);
     audioSource.setVolume(volume);
   }
 
   @override
-  void dispose(){
+  void dispose() {
     super.dispose();
     audioSource.dispose();
   }
 
   @override
   void updateMatrixWorld([bool force = false]) {
-		super.updateMatrixWorld( force );
+    super.updateMatrixWorld(force);
 
-		if (hasPlaybackControl && !isPlaying) return;
+    if (hasPlaybackControl && !isPlaying) return;
 
-		matrixWorld.decompose( _position, _quaternion, _scale );
-		_orientation.setValues( 0, 0, 1 ).applyQuaternion( _quaternion );
+    matrixWorld.decompose(_position, _quaternion, _scale);
+    _orientation.setValues(0, 0, 1).applyQuaternion(_quaternion);
 
     final dist = _position.distanceTo(listner.position);
 
     // Sphere of influence
-    if(dist <= maxDistance){
-      if(_insideAudioArea(_position.clone(),_orientation,listner.position.clone(),coneOuterAngle).inside){ //is inside cone
+    if (dist <= maxDistance) {
+      if (_insideAudioArea(_position.clone(), _orientation,
+              listner.position.clone(), coneOuterAngle)
+          .inside) {
+        //is inside cone
         //check balance
-        _AudioArea aa = _insideAudioArea(_position.clone(),_orientation,listner.position.clone(),coneInnerAngle);
-        
-        final diffAngle = (coneOuterAngle-coneInnerAngle)/2;
-        final double anglePercent = aa.inside?1:(coneOuterAngle/2-aa.angle)/diffAngle;
+        _AudioArea aa = _insideAudioArea(_position.clone(), _orientation,
+            listner.position.clone(), coneInnerAngle);
+
+        final diffAngle = (coneOuterAngle - coneInnerAngle) / 2;
+        final double anglePercent =
+            aa.inside ? 1 : (coneOuterAngle / 2 - aa.angle) / diffAngle;
 
         //check volume
-        if(dist >= refDistance && dist <= maxDistance){
+        if (dist >= refDistance && dist <= maxDistance) {
           //dist to percent
-          final double percent = ((maxDistance-dist)/(maxDistance-refDistance)).toPrecision(rolloffFactor)*anglePercent;
-          
-          if(getVolume() != percent){
+          final double percent =
+              ((maxDistance - dist) / (maxDistance - refDistance))
+                      .toPrecision(rolloffFactor) *
+                  anglePercent;
+
+          if (getVolume() != percent) {
             setVolume(percent);
           }
-        }
-        else if(dist <= refDistance && getVolume() != 1.0){
+        } else if (dist <= refDistance && getVolume() != 1.0) {
           setVolume(1.0);
-        }
-        else if(dist > refDistance && getVolume() != 0.0){
+        } else if (dist > refDistance && getVolume() != 0.0) {
           setVolume(0);
         }
 
-        if(!aa.inside){ ///is not inside little cone
+        if (!aa.inside) {
+          ///is not inside little cone
           // dist from little cone
-          if(getBalance() != anglePercent){
-            setBalance((1-anglePercent)*-aa.side);
+          if (getBalance() != anglePercent) {
+            setBalance((1 - anglePercent) * -aa.side);
           }
-        }
-        else if(getBalance() != 0.0){
+        } else if (getBalance() != 0.0) {
           setBalance(0.0);
         }
-      }
-      else{ // is inside little sphere
-        if(dist <= refDistance){
+      } else {
+        // is inside little sphere
+        if (dist <= refDistance) {
           //dist to percent
-          final double percent = ((refDistance-dist)/refDistance).toPrecision(rolloffFactor);
-          if(getVolume() != percent){
+          final double percent =
+              ((refDistance - dist) / refDistance).toPrecision(rolloffFactor);
+          if (getVolume() != percent) {
             setVolume(percent);
           }
-        }
-        else if(dist == 0 && getVolume() != 1.0){
+        } else if (dist == 0 && getVolume() != 1.0) {
           setVolume(1.0);
-        }
-        else if(dist > refDistance && getVolume() != 0.0){
+        } else if (dist > refDistance && getVolume() != 0.0) {
           setVolume(0);
         }
       }
-    }
-    else if(getVolume() != 0.0){
+    } else if (getVolume() != 0.0) {
       setVolume(0);
     }
-	}
+  }
+
+  /// [meta] - object containing metadata such as textures or images for the scene.
+  ///
+  /// Convert the scene to three.js
+  /// [JSON Object/Scene format](https://github.com/mrdoob/three.js/wiki/JSON-Object-Scene-format-4).
+  @override
+  Map<String, dynamic> toJson({Object3dMeta? meta}) {
+    Map<String, dynamic> data = super.toJson(meta: meta);
+
+    data['audioSource'] = audioSource.toJson();
+    data['rolloffFactor'] = rolloffFactor;
+    data['maxDistance'] = maxDistance;
+    data['refDistance'] = refDistance;
+    data['coneInnerAngle'] = coneInnerAngle;
+    data['coneOuterAngle'] = coneOuterAngle;
+    data['coneOuterGain'] = coneOuterGain;
+
+    return data;
+  }
+
+  @override
+  dynamic getProperty(String propertyName, [int? offset]) {
+    if (propertyName == 'audioSource') {
+      return audioSource;
+    } else if (propertyName == 'refDistance') {
+      return refDistance;
+    } else if (propertyName == 'maxDistance') {
+      return maxDistance;
+    } else if (propertyName == 'rolloffFactor') {
+      return rolloffFactor;
+    } else if (propertyName == 'coneInnerAngle') {
+      return coneInnerAngle;
+    } else if (propertyName == 'coneOuterAngle') {
+      return coneOuterAngle;
+    } else if (propertyName == 'coneOuterGain') {
+      return coneOuterGain;
+    } else if (propertyName == 'listner') {
+      return listner;
+    } else if (propertyName == 'autoplay') {
+      return autoplay;
+    } else if (propertyName == 'loop') {
+      return loop;
+    } else if (propertyName == 'hasPlaybackControl') {
+      return hasPlaybackControl;
+    } else if (propertyName == 'loopEnd') {
+      return loopEnd;
+    } else if (propertyName == 'loopStart') {
+      return loopStart;
+    } else if (propertyName == 'playbackRate') {
+      return playbackRate;
+    } else if (propertyName == 'path') {
+      return path;
+    }
+    return super.getProperty(propertyName, offset);
+  }
+
+  @override
+  PositionalAudio setProperty(String propertyName, dynamic value, [int? offset]) {
+    if (propertyName == 'audioSource') {
+      audioSource = value;
+    } else if (propertyName == 'refDistance') {
+      refDistance = value.toDouble();
+    } else if (propertyName == 'maxDistance') {
+      maxDistance = value.toDouble();
+    } else if (propertyName == 'rolloffFactor') {
+      rolloffFactor = value.toInt();
+    } else if (propertyName == 'coneInnerAngle') {
+      coneInnerAngle = value.toDouble();
+    } else if (propertyName == 'coneOuterAngle') {
+      coneOuterAngle = value.toDouble();
+    } else if (propertyName == 'coneOuterGain') {
+      coneOuterGain = value.toDouble();
+    } else if (propertyName == 'listner') {
+      listner = value;
+    } else if (propertyName == 'autoplay') {
+      autoplay = value;
+    } else if (propertyName == 'loop') {
+      loop = value;
+    } else if (propertyName == 'loopEnd') {
+      loopEnd = value.toInt();
+    } else if (propertyName == 'loopStart') {
+      loopStart = value.toInt();
+    } else if (propertyName == 'playbackRate') {
+      playbackRate = value.toDouble();
+    } else if (propertyName == 'path') {
+      path = value;
+    } else {
+      super.setProperty(propertyName, value);
+    }
+
+    return this;
+  }
 }
